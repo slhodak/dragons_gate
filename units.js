79bf41b45db981c3a25c9b67c8afc884620a28d8
Sam Hodak <slhodak@gmail.com>
2020-11-25 15:47:01 -0800

    Create game rules, story, stats

---
+++ units.js
@@ -0,0 +1,39 @@
+class Unit {
+  constructor(HP, Range, Speed, Attack, Defence, Regen);
+}
+
+class EliteSoldier extends Unit {
+
+}
+
+class FlagBearer extends Unit {
+
+}
+
+class Yuma extends Unit {
+
+}
+
+class Kusarigama extends Unit {
+
+}
+
+class Daisho extends Unit {
+
+}
+
+class Shuriken extends Unit {
+
+}
+
+class Ryu extends Unit {
+
+}
+
+class Yokai extends Unit {
+
+}
+
+class Shinja extends Unit {
+
+}
