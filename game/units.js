ad7d4ad7760849d958ba7b371e819200f2460467
Sam Hodak <slhodak@gmail.com>
2020-11-25 23:17:00 -0800

    Add unit stats to constructors

--- game/units.js
+++ game/units.js
@@ -17,8 +17,8 @@ class EliteSoldier extends Unit {
     let stats = {
       healthPoints: 30,
       speed: 3,
-      range: 2,
-      attackDamage: [2, 4],
+      meleeRange: 2,
+      meleeDamage: [2, 4],
       defenseArmor: [2, 4],
       healthRegen: 2
     };
@@ -29,10 +29,10 @@ class EliteSoldier extends Unit {
 class FlagBearer extends Unit {
   constructor() {
     let stats = {
-      healthPoints: 40,
+      healthPoints: 35,
       speed: 2,
-      range: 1,
-      attackDamage: [2, 4],
+      meleeRange: 1,
+      meleeDamage: [2, 4],
       defenseArmor: [2, 4],
       healthRegen: 3
     };
@@ -42,61 +42,118 @@ class FlagBearer extends Unit {
 
 class Yuma extends Unit {
   constructor() {
-    super();
-  
+    let stats = { 
+      healthPoints: 50,
+      speed: 3,
+      meleeRange: 2,
+      meleeDamage: [4, 6],
+      defenseArmor: [5, 6],
+      healthRegen: 5
+    }
+    super(stats);
   }
   
 }
 
 class Kusarigama extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 50,
+      speed: 3,
+      meleeRange: 3,
+      meleeDamage: [4, 6],
+      defenseArmor: [5, 5],
+      healthRegen: 5
+    }
+    super(stats);
   }
   
 }
 
 class Daisho extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 50,
+      speed: 3,
+      meleeRange: 2,
+      meleeDamage: [4, 6],
+      defenseArmor: [5, 5],
+      healthRegen: 5
+    }
+    super(stats);
   }
   
 }
 
 class Shuriken extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 50,
+      speed: 4,
+      meleeRange: 2,
+      meleeDamage: [4, 6],
+      rangedRange: 10,
+      // -1 roll per 2cm distance
+      // e.g. at 5cm, rangedDamage = [4, 4]
+      rangedDamage: [6, 4],
+      defenseArmor: [5, 5],
+      healthRegen: 5
+    }
+    super(stats);
+
   }
   
 }
 
 class Ryu extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 100,
+      speed: 0,
+      meleeRange: 3,
+      meleeDamage: [10, 10],
+      rangedRange: Infinity,
+      rangedDamage: [4, 4],
+      defenseArmor: [4, 4],
+      healthRegen: 5
+    }
+    super(stats);
   }
   
 }
 
 class Yokai extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 60,
+      speed: 5,
+      meleeRange: 3,
+      meleeDamage: [2, 10],
+      defenseArmor: [3, 4],
+      healthRegen: 2
+    }
+    super(stats);
   }
   
 }
 
 class Shinja extends Unit {
-  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
-    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
-  
+  constructor() {
+    let stats = { 
+      healthPoints: 20,
+      speed: 2,
+      meleeRange: 2,
+      meleeDamage: [3, 6],
+      defenseArmor: [4, 4],
+      healthRegen: 0
+    }
+    super(stats);
   }
 
 }
 
 module.exports = {
+  Unit,
   EliteSoldier,
   FlagBearer,
   Yuma,
