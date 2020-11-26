37ba2e5327e864460b58d06a32f7e6ebf8481202
Sam Hodak <slhodak@gmail.com>
2020-11-26 00:21:54 -0800

    Save game state on server

--- game/game.js
+++ game/game.js
@@ -1,14 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
 const { Empire, Protectors, Guardians } = require('./factions.js');
 
 // Tracks the state of factions and their units
 // Interface for users to play the game
 class Game {
   constructor() {
-    this.factions = [ new Empire(), new Protectors(), new Guardians() ];
+    this.factions = {
+      empire: new Empire(),
+      protectors: new Protectors(),
+      guardians: new Guardians()
+    }
   }
   // Alter defender's HP (and maybe state) based on calculated damage (and maybe other effects)
   doCombat(attacker, defender) {
 
   }
-
+  // Write game state to file
+  async save() {
+    const stateFilePath = path.join(__dirname, './state.json');
+    await fs.writeFile(stateFilePath, JSON.stringify(this.factions));
+  }
 }
+
+module.exports = () => new Game();
--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const port = 3456;
 
+const game = require('../game/game.js')();
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.listen(port, () => {
-  `Dragon's Gate server bound to port ${port}`;
+  game.save();
+  console.debug(`Dragon's Gate server bound to port ${port}`);
 });
