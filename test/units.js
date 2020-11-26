8e3535112d0af037481e7d2453a280bcaa5fd1fa
Sam Hodak <slhodak@gmail.com>
2020-11-25 20:50:57 -0800

    use .equal and 2 asserts

--- test/units.js
+++ test/units.js
@@ -1,22 +1,33 @@
 const assert = require('assert');
-const units = require('../game/units.js');
+const {
+  EliteSoldier,
+  FlagBearer,
+  Yuma,
+  Kusarigama,
+  Daisho,
+  Shuriken,
+  Ryu,
+  Yokai,
+  Shinja
+} = require('../game/units.js');
 
 describe('Unit', () => {
   describe('EliteSoldier', () => {
     before(() => {
-      this.eliteSoldier = new units.EliteSoldier();
+      this.eliteSoldier = new EliteSoldier();
     });
     describe('#roll', () => {
       it('should return an integer', () => {
         let { attackDamage } = this.eliteSoldier.stats;
         let damage = this.eliteSoldier.roll(attackDamage);
-        assert(damage % 1 == 0)
+        assert.equal(damage % 1, 0)
       });
       it('should return a valid roll value based on the input', () => {
         let { attackDamage } = this.eliteSoldier.stats;
         let maxDamage = attackDamage[0] * attackDamage[1];
         let damage = this.eliteSoldier.roll(attackDamage);
-        assert(damage > 0 && damage < maxDamage);
+        assert(damage > 0);
+        assert(damage < maxDamage)
       });
     });
   });
