d8f7d6378e7e78379cd5fb52ba00800d27d9328b
Sam Hodak <slhodak@gmail.com>
2020-11-25 19:36:33 -0800

    Develop unit roll method with associated test

---
+++ test/units.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const units = require('../units.js');
+
+describe('Unit', () => {
+  describe('EliteSoldier', () => {
+    before(() => {
+      this.eliteSoldier = new units.EliteSoldier();
+    });
+    describe('#roll', () => {
+      it('should return an integer', () => {
+        let { attackDamage } = this.eliteSoldier.stats;
+        let damage = this.eliteSoldier.roll(attackDamage);
+        assert(damage % 1 == 0)
+      });
+      it('should return a valid roll value based on the input', () => {
+        let { attackDamage } = this.eliteSoldier.stats;
+        let damageSplit = attackDamage.split('d');
+        let maxDamage = damageSplit[0] * damageSplit[1];
+        let damage = this.eliteSoldier.roll(attackDamage);
+        assert(damage > 0 && damage < maxDamage);
+      });
+    });
+  });
+});
--- units.js
+++ units.js
@@ -1,39 +1,98 @@
 class Unit {
-  constructor(HP, Range, Speed, Attack, Defence, Regen);
+  constructor(stats) {
+    this.stats = stats;
+  }
+  roll(xdy) {
+    // parse rolls-d-sides string and calculate resulting damage
+    const xdy_split = xdy.split('d');
+    const rolls = xdy_split[0];
+    const sides = xdy_split[1];
+    let total = 0;
+    for (let i = 0; i < rolls; i++) {
+      total += Math.ceil(Math.random() * sides);
+    }
+    return total;
+  }
 }
 
 class EliteSoldier extends Unit {
-
+  constructor() {
+    let stats = { healthPoints: 30, speed: 3, range: 2, attackDamage: "2d4", defenseArmor: "2d4", healthRegen: 2 };
+    super(stats);
+  }
 }
 
 class FlagBearer extends Unit {
-
+  constructor() {
+    let stats = { healthPoints: 40, speed: 2, range: 1, attackDamage: "2d4", defenseArmor: "3d4", healthRegen: 3 };
+    super(stats);
+  }
 }
 
 class Yuma extends Unit {
-
+  constructor() {
+    super();
+  
+  }
+  
 }
 
 class Kusarigama extends Unit {
-
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
+  
 }
 
 class Daisho extends Unit {
-
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
+  
 }
 
 class Shuriken extends Unit {
-
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
+  
 }
 
 class Ryu extends Unit {
-
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
+  
 }
 
 class Yokai extends Unit {
-
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
+  
 }
 
 class Shinja extends Unit {
+  constructor(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen) {
+    super(healthPoints, speed, range, attackDamage, defenseArmor, healthRegen);
+  
+  }
 
 }
+
+module.exports = {
+  EliteSoldier,
+  FlagBearer,
+  Yuma,
+  Kusarigama,
+  Daisho,
+  Shuriken,
+  Ryu,
+  Yokai,
+  Shinja
+};
