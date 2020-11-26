1408ee8e1e9beb932025ea2442d45c88e9be5a53
Sam Hodak <slhodak@gmail.com>
2020-11-26 00:29:50 -0800

    Add save route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const port = 3456;
 const game = require('../game/game.js')();
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+app.post('save', (req, res) => {
+  let saved = game.save();
+  if (saved) {
+    res.send(200);
+  } else {
+    res.status(400).send(`Error saving game: ${saved}`);
+  }
+  
+});
+
 app.listen(port, () => {
   game.save();
   console.debug(`Dragon's Gate server bound to port ${port}`);
