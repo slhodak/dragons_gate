cb13d1606c7bf84e659b54a2fe8f97769eabe52b
Sam Hodak <slhodak@gmail.com>
2020-11-26 22:32:34 -0800

    Remove debug div

--- client/components/Factions.js
+++ client/components/Factions.js
@@ -6,7 +6,6 @@ export default (props) => {
   const { factions } = props;
   return (
     <div className="factions">
-      {`${Object.keys(factions).length} factions loaded`}
       {Object.keys(factions).map(faction => <Faction faction={factions[faction]} name={faction} selectCombatant={props.selectCombatant} />)}
     </div>
   )
