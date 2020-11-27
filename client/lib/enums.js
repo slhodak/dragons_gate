f21ff18ef5fb1d009c59723939c5951e3cabe528
Sam Hodak <slhodak@gmail.com>
2020-11-27 12:20:48 -0800

    Create enums for constant values

---
+++ client/lib/enums.js
@@ -0,0 +1,14 @@
+const attackTypes = {
+  RANGED: 'ranged',
+  MELEE: 'melee'
+}
+
+const combatantTypes = {
+  ATTACKER: 'attacker',
+  DEFENDER: 'defender'
+}
+
+export {
+  attackTypes as attackTypes,
+  combatantTypes as combatantTypes
+}
