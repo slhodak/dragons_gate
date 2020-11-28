d04968f5a26570c295f730978329a491ef5009ed
Sam Hodak <sam.hodak@tact.ai>
2020-11-28 12:59:29 -0800

    Add footer

---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style.css';
+
+export default (props) => {
+  return (
+    <div className="footer">
+      <div><a target="_blank" href="https://icons8.com/icons/set/dragon">Dragon icon</a> and</div>
+      <div><a target="_blank" href="https://icons8.com/icons/set/katana">Katana icon</a> by <a target="_blank" href="https://icons8.com">Icons8</a></div>
+    </div>
+  )
+};
