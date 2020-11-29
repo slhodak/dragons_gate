7855bdb28b3c5fb7cde84d5df2036673235eb7d3
Sam Hodak <sam.hodak@tact.ai>
2020-11-28 16:50:23 -0800

    Remove extraneous messages

--- client/components/AttackButton.js
+++ client/components/AttackButton.js
@@ -14,14 +14,13 @@ export default (props) => {
           return <button onClick={() => props.selectAttacker(unit, attackType)}>{capitalize(attackType)} Attack</button>
         }
         if (attacker.id === unit.id) {
-          return <div>Select enemy or <button onClick={props.resetAttack}>Cancel</button></div>;
+          return <div>Select enemy or <button className="cancelButton" onClick={props.resetAttack}>Cancel</button></div>;
         }
         if (!defender) {
           if (attacker.faction === unit.faction) {
             return <div><em>Selecting defender</em></div>;
           }
         }
-        return <div><em>Confirming attack</em></div>
       })()}
     </div>
   )
