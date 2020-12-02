e41c75321873631748c3a7d65971f8f87588469c
Sam Hodak <sam.hodak@tact.ai>
2020-12-01 22:25:26 -0600

    Ask question

--- test/client/__mocks__/factionMocks.js
+++ test/client/__mocks__/factionMocks.js
@@ -1,3 +1,4 @@
+// Are these mocks? These are real factions
 const { EliteSoldier } = require(`${process.env.PWD}/server/game/units.js`);
 const { Empire, Protectors } = require(`${process.env.PWD}/server/game/factions.js`);
 
