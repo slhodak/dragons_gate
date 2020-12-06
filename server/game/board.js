71cacc7b5f9eacaa649662d74172ecf9f8b90bcd
Sam Hodak <slhodak@gmail.com>
2020-12-05 17:48:17 -0800

    Simplify pathsback

--- server/game/board.js
+++ server/game/board.js
@@ -14,36 +14,19 @@ const InverseNeighbor = {
 // Can I have concept of "two rights" or "two lefts"?
 //  "Two turns in the same direction"
 // "Turn" right or left -- needs to know initial direction. bottom -> top, right == bottomRight
-const PathsBackToMe = {
-  top: { 
-    bottomRight: 'bottomLeft', // use in InverseNeighbor to find my position
-    bottomLeft: 'bottomRight'
-  },
-  topRight: {
-    topLeft: 'bottom',
-    bottom: 'topLeft'
-  },
-  bottomRight: {
-    top: 'bottomLeft',
-    bottomLeft: 'top'
-  },
-  bottom: {
-    topRight: 'topLeft',
-    topLeft: 'topRight'
-  },
-  bottomLeft: {
-    bottomRight: 'top',
-    top: 'bottomRight'
-  },
-  topLeft: {
-    bottom: 'topRight',
-    topRight: 'bottom'
-  }
+
+// Moving to each one once returns to cell
+const PathsBackToCell = {
+  a: [ 'top', 'bottomRight', 'bottomLeft' ],  // delta
+  b: [ 'topRight', 'topLeft', 'bottom'] // inverted delta
 };
 
 // once the whole board is made, lace up all connections
 // while trying to make neighbors, if they already exist, lace connection
 //    how to know if they already exist?
+//    The one we come from exists
+//      The one we started at exists (cell of PathsBackToCell)
+
 
 // Make all rings (use ring size/side-length algorithm)
 //    ringSize = ringNumber * 6
@@ -51,6 +34,12 @@ const PathsBackToMe = {
 // Connect each ring with the sizes bordering within
 //    Inner neighbors to connect per ring = (ringNumber * 2) - 1
 
+// Following the paths back to me, create and or connect neighbors
+//    Lace forward and reverse as you go
+// First do a from 0, then a from 1, then a from 2
+//    Then b from 0, 1, 2
+//  Then start at top of current cell and make that current cell
+//    // create and/or lace 
 
 class Cell {
   constructor(boardSize, distanceFromCenter, neighbors) {
