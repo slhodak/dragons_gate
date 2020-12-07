a2efd486f3d5848f701871ebc79276964624b7b1
Sam Hodak <slhodak@gmail.com>
2020-12-06 19:53:58 -0800

    Lots of work, some progress on hexes...

--- server/game/board.js
+++ server/game/board.js
@@ -1,69 +1,73 @@
-// Who is the root node? Center?
+// Hexagonal hex board
 
-// The position of a neighbor by their position for you
-const InverseNeighbor = {
-  top: 'bottom',
-  topRight: 'bottomLeft',
-  bottomRight: 'topLeft',
-  bottom: 'top',
-  bottomLeft: 'topRight',
-  topLeft: 'bottomRight'
+const moves = {
+  TOP_RIGHT: 'topRight',
+  BOTTOM_RIGHT: 'bottomRight',
+  BOTTOM: 'bottom',
+  BOTTOM_LEFT: 'bottomLeft',
+  TOP_LEFT: 'topLeft',
+  TOP: 'top'
 };
 
-// I am the last position in the path
-// Can I have concept of "two rights" or "two lefts"?
-//  "Two turns in the same direction"
-// "Turn" right or left -- needs to know initial direction. bottom -> top, right == bottomRight
-
-// Moving to each one once returns to cell
-const PathsBackToCell = {
-  a: [ 'top', 'bottomRight', 'bottomLeft' ],  // delta
-  b: [ 'topRight', 'topLeft', 'bottom'] // inverted delta
+// The position of a neighbor by your position to them
+const inverseNeighbor = {
+  top: moves.BOTTOM,
+  topRight: moves.BOTTOM_LEFT,
+  bottomRight: moves.BOTTOM_RIGHT,
+  bottom: moves.TOP,
+  bottomLeft: moves.TOP_RIGHT,
+  topLeft: moves.TOP_LEFT
 };
 
-// once the whole board is made, lace up all connections
-// while trying to make neighbors, if they already exist, lace connection
-//    how to know if they already exist?
-//    The one we come from exists
-//      The one we started at exists (cell of PathsBackToCell)
-
-
-// Make all rings (use ring size/side-length algorithm)
-//    ringSize = ringNumber * 6
-//    sideLength = ringNumber
-// Connect each ring with the sizes bordering within
-//    Inner neighbors to connect per ring = (ringNumber * 2) - 1
-
-// Following the paths back to me, create and or connect neighbors
-//    Lace forward and reverse as you go
-// First do a from 0, then a from 1, then a from 2
-//    Then b from 0, 1, 2
-//  Then start at top of current cell and make that current cell
-//    // create and/or lace 
+// Moving to each one once returns to current cell
+const pathsBackToCell = [
+  // I think this order matters for makeNeighbors
+  [ moves.TOP_RIGHT, moves.BOTTOM, moves.TOP_LEFT ],
+  [ moves.BOTTOM_RIGHT, moves.BOTTOM_LEFT, moves.TOP ]
+];
 
 class Cell {
-  constructor(boardSize, distanceFromCenter, neighbors) {
-    this.boardSize = boardSize;
-    this.distanceFromCenter = distanceFromCenter;
+  constructor(board, ring, neighbors = {}) {
+    this.ring = ring;
+    this.board = board;
     this.neighbors = neighbors;
-    this.makeNeighbors(boardSize);
   }
+  // makeNeighbors
+  // Create and/or connect all neighbors
+  // Follow around Pathsback alternating
+  //    a 0, 1, 2 then b 0, 1, 2 then a 1, 2, 0, etc.
+  //    Connect forward and reverse
+  //    Start again with a[0] as current cell
   makeNeighbors() {
-    if (this.distanceFromCenter >= this.boardSize) {
+    if (this.ring > this.board.diameter) {
       return;
     }
-    // Create "breadth-first"
-    // Make each new & existing neighbor aware of all
-    //  new & existing neighbors bordering it
-
-
+    // keep track of original center
+    // move current cell around PathsBack
+    for (let i = 0; i < 6; i++) {
+      let pathSet = i % 2;
+      for (let j = 0; j < 3; j++) {
+        let moveIndex = (i + j) % 3;
+        let move = pathsBackToCell[pathSet][moveIndex];
+        if (!this.neighbors[move]) {
+          this.neighbors[move] = new Cell(this.board, this.ring + 1, {})
+          this.neighbors[move].setNeighbor(this, move);
+        }
+      }
+    }
+  }
+  makeNeighborsAlongPath(move/path)??
+  // neighbor: instance of Cell, myPositionToThem: member of "moves"
+  setNeighbor(neighbor, myPositionToThem) {
+    const theirPositionToMe = inverseNeighbor[myPositionToThem];
+    this.neighbors[theirPositionToMe] = neighbor;
   }
 }
 
 class Board {
-  constructor(size) {
-    this.size = size;
-    this.center = new Cell(size, 0);
+  constructor(diameter) {
+    this.diameter = diameter;
+    this.center = new Cell(this, 0);
     this.center.makeNeighbors();
   }
 }
